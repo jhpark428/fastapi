e7f3d1a68555d0e23234ea30095cb0d0abd457ab
Jeahyun Park <jhpark428@gmail.com>
2024-12-27 09:16:34 +0900

    introduced favicon.ico

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import argparse
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+favicon_path = "./favicon.ico"
 
 @app.get("/")
 async def index():
   return {"message": "Hello, FastAPI!!"}
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
